e88b6bd5087e5b2da512bb14c3024e45c410510a
Mike Rourke <mike.w.rourke@gmail.com>
2017-02-04 18:39:59 +0000

    Added contingency for Cloud9 to update client script
    The existing updateClient script only accommodated for a root directory named Repositories.  I added an if statement to search for workspace if working on Cloud9.

--- tools/updateClient.sh
+++ tools/updateClient.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # Copies the bundled front-end into the "src" directory of the project
 # for testing.
-cd "`dirname $0`"/../
+cd "`dirname $0`"/../src
 rm -rf client && mkdir client
-cd ~/Repositories
+
+# This is to ensure the script works in Cloud9:
+if [ -d "~/Repositories" ]; then
+    cd ~/Repositories
+else 
+    cd ~/workspace
+fi
+
 cp -r square1-web/client square1-api/src
